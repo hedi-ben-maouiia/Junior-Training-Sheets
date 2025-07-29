800d9248843e8c06170513c303bb469db335bc9a
hedi-ben-maouiia <teedydeev@gmail.com>
2025-07-29 11:53:27 +0100

    back to solving problems

--- code_forces/is_it_rated.cpp
+++ code_forces/is_it_rated.cpp
@@ -1,22 +1,27 @@
-#include <iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    int n,a,b,tmp=5000; 
+    int n,a,b;
     cin >> n;
-    while(n--){
+    vector<int> v;
+    int t = n;
+    while(t--)
+    {
         cin >> a >> b;
-        if(a!=b){
-            cout << "\nrated\n";
+        v.push_back(a); 
+        if(a != b){
+            cout << "rated\n";
             return 0;
         }
-        if(a > tmp){
+    }
+    for(int i = 1; i < n; i++){
+        if(v[i] > v[i-1]){
             cout << "unrated\n";
             return 0;
         }
-        tmp = a;
     }
     cout << "maybe\n";
     return 0;
